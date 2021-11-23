d0aed626c60b1b987b6208b2a401e92d8055cc26
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-11-23 10:33:09 -0700

    adding apiRoutes post code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
-const { readFromFile } = require('../helper/helper');
+var uniqid = require('uniqid');
+const { readFromFile, readAndAppend } = require('../helper/helper');
 
 router.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) =>
     res.json(JSON.parse(data))
   )
     .catch((err) => console.log(err))
+});
+
+router.post('/api/notes', (req, res) => {
+  const newNote= req.body
+  newNote.id = uniqid()
+
+     readAndAppend(newNote, './db/db.json');
+
+     res.json(newNote);
+ 
 });
 
 module.exports = router;
