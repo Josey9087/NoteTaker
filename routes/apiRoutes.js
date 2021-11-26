71a2d8290fae3237b6cc1acd1186b3ac38aa8727
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2021-11-26 12:02:04 -0700

    adding delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 var uniqid = require('uniqid');
-const { readFromFile, readAndAppend } = require('../helper/helper');
+const { readFromFile, readAndAppend, writeToFile } = require('../helper/helper');
 
 router.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) =>
@@ -10,13 +10,24 @@ router.get('/api/notes', (req, res) => {
 });
 
 router.post('/api/notes', (req, res) => {
-  const newNote= req.body
+  const newNote = req.body
   newNote.id = uniqid()
 
-     readAndAppend(newNote, './db/db.json');
+  readAndAppend(newNote, './db/db.json');
 
-     res.json(newNote);
- 
+  res.json(newNote);
+
+});
+
+router.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  readFromFile('./db/db.json').then((data) => {
+    Notes = JSON.parse(data).filter(data => data.id != id);
+    const newNote = writeToFile('./db/db.json', Notes);
+    res.json(newNote);
+  }
+  )
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
